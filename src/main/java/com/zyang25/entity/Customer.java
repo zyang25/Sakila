af1aa50b47e7c9e0b081b0cded31878b3b777041
Zi <zyang25@stevens.edu>
2018-02-13 23:49:20 -0500

    add entities

--- src/main/java/com/zyang25/entity/Customer.java
+++ src/main/java/com/zyang25/entity/Customer.java
@@ -1,4 +1,33 @@
 package com.zyang25.entity;
 
+import org.hibernate.annotations.GeneratorType;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int customer_id;
+
+    @OneToMany
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    private String first_name;
+
+    private String last_name;
+
+    private String email;
+
+    private int address_id;
+
+    private int active;
+
+    private Date create_date;
+
+    private Date last_update;
+
 }
