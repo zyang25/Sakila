5c563ef2fb5c636c025d718671868f1254987aa2
Zi <zyang25@stevens.edu>
2018-02-13 23:21:04 -0500

    Init. Spring boot app

---
+++ src/main/java/com/zyang25/sakila/SakilaApplication.java
@@ -0,0 +1,12 @@
+package com.zyang25.sakila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SakilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SakilaApplication.class, args);
+	}
+}
